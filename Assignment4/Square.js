d0dd5e2088a45e1ad1bd6389bf5238a251a12587
BThompson1992 <35515592+BThompson1992@users.noreply.github.com>
2018-02-06 20:08:41 -0800

    Update Square.js

--- Assignment4/Square.js
+++ Assignment4/Square.js
@@ -28,9 +28,9 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	};
 	this.colors = {
 		values : new Float32Array([
-		    1.0, 0.0, 0.0, 
-		    1.0, 0.0, 0.0,
-		    1.0, 0.0, 0.0,
+		    0.0, 1.0, 0.0, 
+		    0.5, 0.0, 0.5,
+		    0.25, 0.25, 0.25,
 		    1.0, 0.0, 0.0
 		]),
 		numComponents : 3 
