dfc00c1535ccf37eb7a613274df140d46593b620
BThompson1992 <35515592+BThompson1992@users.noreply.github.com>
2018-02-06 20:54:26 -0800

    Update Square.js

--- Assignment4/Square.js
+++ Assignment4/Square.js
@@ -88,12 +88,12 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	};
     this.indices = {
 	    //index to vertices
-    values : new Uint16Array([ 0, 1, 2, 0, 2, 3, 
-			       4, 5, 6, 4, 6, 7,
-			       8, 9, 10, 8, 10, 11,
-			       12, 13, 14, 12, 14, 15,
-			       16, 17, 18, 16, 18, 19,
-			       20, 21, 22, 20, 22, 23])
+    values : new Uint16Array([ 0, 1, 2, 0, 2, 3,        //Front Face
+			       4, 5, 6, 4, 6, 7,        //Back Face
+			       8, 9, 10, 8, 10, 11,     //Top Face 
+			       12, 13, 14, 12, 14, 15,  //Bottom Face
+			       16, 17, 18, 16, 18, 19,  //Left Face
+			       20, 21, 22, 20, 22, 23]) //Right Face
     };
 	
 	// positions
