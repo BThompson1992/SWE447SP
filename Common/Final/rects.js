02f24b513f0d5439a75fc4afdc79276b4f8b3c0d
BThompson1992 <35515592+BThompson1992@users.noreply.github.com>
2018-03-19 13:13:06 -0700

    Update rects.js

--- Common/Final/rects.js
+++ Common/Final/rects.js
@@ -1,16 +1,17 @@
 function rect(){
  
-  var width;
-  var depth;
-  var length;
-  var Xpos;
-  var Ypos;
-  var Zpos;
+  this.width = 5;
+  this.depth = 5;
+  this.length = 5;
+  this.Xpos = 0;
+  this.Ypos = 0;
+  this.Zpos = 0;
+ 
+ 
   
 }
 
 rect.prototype.resize(){
  
-  
   
 }
