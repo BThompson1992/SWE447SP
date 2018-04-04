f727d355ff05d52ff756d9a493757b7f58be81b8
BThompson1992 <35515592+BThompson1992@users.noreply.github.com>
2018-04-03 21:00:50 -0700

    Update rects.js

--- Common/Final/rects.js
+++ Common/Final/rects.js
@@ -4,7 +4,7 @@ function rect(){
   this.Ypos = 0;
   this.Zpos = 0;
  
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00} );
   this.cube = new THREE.Mesh( geometry, material );
   
